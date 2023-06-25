b51f672dd8926fe3fc16c031961ebbd15764406e
Gio <lotuspar0@gmail.com>
2023-06-26 02:57:56 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <h1>hi</h1>
+    </div>
+</div>

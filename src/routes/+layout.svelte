2cb4eacff1159cceaa2fb2076dc5f801652fe20f
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-06-26 09:39:39 +0800

    First website iteration - nameplate done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,72 @@
     import "../app.css";
 </script>
 
-<slot />
+<style>
+    .oca-background-container {
+        position: fixed;
+        z-index: -5;
+
+        width: 100%;
+        height: 100%;
+
+        background-color: black;
+    }
+
+    .oca-background-item {
+        position: absolute;
+
+        width: 100%;
+        height: 100%;
+    }
+
+    .oca-background-item.content {
+        z-index: -4;
+
+        object-fit: cover;
+
+        transform: scale(1.5);
+    }
+
+    .oca-background-item.content video {
+        width: 100%;
+        height: 100%;
+
+        object-fit: fill;
+    }
+
+    .oca-background-item.overlay {
+        z-index: -3;
+
+        -webkit-backdrop-filter: blur(40px);
+        backdrop-filter: blur(40px);
+
+        background-color: rgba(250, 250, 250, 0.7);
+    }
+
+    @media (prefers-color-scheme: light) {
+        .oca-background-item.overlay {
+            background-color: rgba(250, 250, 250, 0.7);
+        }
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .oca-background-item.overlay {
+            -webkit-backdrop-filter: blur(45px);
+            backdrop-filter: blur(45px);
+            background-color: rgba(30, 30, 30, 0.8);
+        }
+    }
+</style>
+
+<div class="oca-background-container">
+    <div class="oca-background-item content">
+        <video autoplay muted loop playsinline>
+            <source src="backgrounds/videos/pb1.mp4" type="video/mp4"/>
+        </video>
+    </div>
+
+    <div class="oca-background-item overlay">
+    </div>
+</div>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,26 @@
-<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-    <div class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-        <h1>hi</h1>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+<div class="relative flex flex-col justify-center items-center min-h-screen">
+    <div class="absolute inset-0 opacity-30 dark:opacity-20 dark:invert bg-[length:100px_100px] bg-[url(/backgrounds/dot-seamless-pattern.svg)] bg-center"></div>
+
+    <div class="relative bg-white px-6 py-8 shadow-xl ring-1 ring-gray-900/5 rounded-lg text-gray-900">
+        <div class="flex flex-row items-center space-x-4">
+            <img class="w-32 h-32 rounded-full shadow-xl ring-1 ring-gray-900/5" src="picture.jpeg" alt="profile pic"/>
+            <div class="flex flex-col space-y-2">
+                <p class="h-6 text-2xl font-medium">Gio</p>
+                <p class="h-4 text-base font-light">developer / ui designer</p>
+                <p class="h-4 text-base font-light">perth, wa</p>
+            </div>
+        </div>
+
+        <div class="flex flex-row justify-center space-x-4 pt-4">
+            <a href="mailto:lotuspar0@gmail.com">
+                <span class="material-icons text-gray-800 hover:text-gray-400">email</span>
+            </a>
+            <a href="https://github.com/lotuspar">
+                <span class="material-icons text-gray-800 hover:text-gray-400">data_object</span>
+            </a>
+        </div>
     </div>
 </div>
